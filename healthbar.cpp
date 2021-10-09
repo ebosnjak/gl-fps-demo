fae554377160cef861c86813170cc9e277d05811
ebosnjak <eugen3141@gmail.com>
2021-10-09 21:42:13 +0200

    Added a new class for a healthbar that is drawn as a ui element and not somewhere in the world

--- healthbar.cpp
+++ healthbar.cpp
@@ -57,3 +57,35 @@ void Healthbar::Draw(ShaderProgram& prog) {
     Content::Instance().GetMesh("rectangle")->Draw(prog);
     prog.SetInt("isViewmodel", 0);
 }
+
+
+UI_Healthbar::UI_Healthbar() : Healthbar() {
+
+}
+
+UI_Healthbar::UI_Healthbar(Entity* _owner, glm::vec2 _size, glm::vec2 _pos, glm::vec3 _fg, glm::vec3 _bg) : Healthbar(_owner, _size, _fg, _bg) {
+    position = _pos;
+}
+
+void UI_Healthbar::Update(float deltaTime) {
+    Healthbar::Update(deltaTime);
+}
+
+void UI_Healthbar::Draw(ShaderProgram& prog) {
+    if (owner == nullptr || !owner->isAlive) {
+        return;
+    }
+    
+    prog.Use();
+    prog.SetInt("useTexture", 0);
+    prog.SetMat4("model", glm::translate(glm::mat4(1.0f), glm::vec3(position, 0.0f)) *
+                          glm::scale(glm::mat4(1.0f), glm::vec3(size, 1.0f)));
+    Content::Instance().GetMesh("rectangle_topleft")->color = bgColor;
+    Content::Instance().GetMesh("rectangle_topleft")->Draw(prog);
+
+    float width = size.x * currentValue / (float)owner->maxHealth;
+    prog.SetMat4("model", glm::translate(glm::mat4(1.0f), glm::vec3(position, 0.007f)) * 
+                          glm::scale(glm::mat4(1.0f), glm::vec3(width, size.y, 1.0f)));
+    Content::Instance().GetMesh("rectangle_topleft")->color = fgColor;
+    Content::Instance().GetMesh("rectangle_topleft")->Draw(prog);
+}
--- healthbar.h
+++ healthbar.h
@@ -24,4 +24,14 @@ struct Healthbar {
     void Draw(ShaderProgram& prog);
 };
 
+struct UI_Healthbar : public Healthbar {
+    glm::vec2 position;
+
+    UI_Healthbar();
+    UI_Healthbar(Entity* _owner, glm::vec2 _size, glm::vec2 _pos = glm::vec2(0.0f), glm::vec3 _fg = glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3 _bg = glm::vec3(1.0f));
+
+    void Update(float deltaTime);
+    void Draw(ShaderProgram& prog);
+};
+
 #endif
